664f9b0fcf7146ab0e8dd9ce9c7dbdcef81d1050
tomsaalex <tomsaalex@gmail.com>
2021-03-11 10:54:29 +0200

    Started work on the save system. Structs should be ready and Newtonsoft is installed.

---
+++ PlanetGen/Assets/UI Scripts/SaveSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+
+public class SaveSystem : MonoBehaviour
+{
+
+
+    
+    void Start()
+    {
+    }
+
+    struct Planet
+    {
+        string name;
+        int resolution;
+        float radius;
+
+        int noiseLayersLength;
+        NoiseLayer[] noiseLayers;
+    }
+
+    struct NoiseLayer
+    {
+        bool enabled;
+        bool useFirstLayerAsMask;
+
+        NoiseSettings noiseSettings;
+    }
+
+    struct NoiseSettings
+    {
+        string filterType;
+
+        float strength;
+        int numLayers;
+        float baseRoughness;
+        float persistence;
+
+        float CenterX;
+        float CenterY;
+        float CenterZ;
+
+        float minValue;
+        float weightMultiplier;
+    }
+}
