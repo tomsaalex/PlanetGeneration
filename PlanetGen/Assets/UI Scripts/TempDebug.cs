a9a7b1657e6cf7121ac7f68426e46a6d1f9079f8
tomsaalex <tomsaalex@gmail.com>
2021-05-13 11:12:30 +0300

    Path showing

---
+++ PlanetGen/Assets/UI Scripts/TempDebug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TempDebug : MonoBehaviour
+{
+    public Text t; 
+    public void UpdateTextThing()
+    {
+        t.text = SaveSystem.path;
+    }
+    public void Update()
+    {
+        UpdateTextThing();
+    }
+}
